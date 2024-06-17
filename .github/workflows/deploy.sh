db7e0d88e8508683d1413818ef8d0016759deb20
Jay Varner <jayvarner@gmail.com>
2024-06-17 14:45:18 -0400

    Update deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,7 +7,12 @@ aws ecr get-login-password --region us-east-1 | \
 docker login --username AWS --password-stdin "${AWS_ECR}"
 echo "Logged in successfully"
 
-docker build -t georgia-coast-atlas --no-cache .
+echo "Building Docker image for ${BRANCH}"
+if [ "$BRANCH" == "main" ]; then
+  docker build -t georgia-coast-atlas --no-cache .
+else
+  docker build -t georgia-coast-atlas --no-cache --file Dockerfile-dev .
+fi
 
 echo "Tagging image with ${TAG}"
 docker tag georgia-coast-atlas "${AWS_ECR}/georgia-coast-atlas:${TAG}"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,40 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        black: "#1C1817",
+        white: "#FDF9F6",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        html: {
+          p: {
+            lineHeight: "1.75rem",
+            marginTop: "1.25rem",
+            marginBottom: "1.25rem",
+          },
+          main: {
+            "p:first-of-type:first-letter": {
+              float: "left",
+              fontFeatureSettings: '"ss06" !important',
+              fontSize: "6rem",
+              lineHeight: "3.5rem",
+              fontWeight: "bold",
+              padding: "1rem 0.75rem 0rem 0rem",
+            },
+          },
+        },
+      });
+    }),
+  ],
 } satisfies Config;
