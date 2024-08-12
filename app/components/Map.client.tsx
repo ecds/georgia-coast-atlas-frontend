9c47995f138a47fafaae4e2858aa2043bfeb76d6
Varshith <varshith.sai0605@gmail.com>
2024-08-12 15:23:27 +0530

    Updated Map client

--- app/components/Map.client.tsx
+++ app/components/Map.client.tsx
@@ -1,10 +1,9 @@
 import maplibregl from "maplibre-gl";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import style from "~/data/style.json";
 import { topBarHeight } from "~/config";
 import "maplibre-gl/dist/maplibre-gl.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLayerGroup } from "@fortawesome/free-solid-svg-icons";
+import MapSwitcher from "./MapSwitcher";
 import type { Dispatch, SetStateAction } from "react";
 import type { StyleSpecification } from "maplibre-gl";
 
@@ -15,8 +14,6 @@ interface Props {
 }
 
 const Map = ({ map, setMap, setMapLoaded }: Props) => {
-  const [activeLayer, setActiveLayer] = useState<string>("custom");
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const mapContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -37,55 +34,7 @@ const Map = ({ map, setMap, setMapLoaded }: Props) => {
     _map.fitBounds(bounds);
 
     _map.on("load", () => {
-      _map.addSource('google-satellite', {
-        type: 'raster',
-        tiles: ['https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'],
-        tileSize: 256,
-        attribution: '© Google',
-      });
-
-      _map.addLayer({
-        id: 'google-satellite-layer',
-        type: 'raster',
-        source: 'google-satellite',
-        layout: {
-          visibility: 'none'
-        }
-      });
-
-      _map.addSource('usgs-topo', {
-        type: 'raster',
-        tiles: ['https://basemap.nationalmap.gov/arcgis/services/USGSTopo/MapServer/WMSServer?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=0&styles=default'],
-        tileSize: 256,
-        attribution: '© USGS',
-      });
-
-      _map.addLayer({
-        id: 'usgs-topo-layer',
-        type: 'raster',
-        source: 'usgs-topo',
-        layout: {
-          visibility: 'none'
-        }
-      });
-
-      // The ATLMaps Layer (I've added it for reference on example for our geoserver)
-      _map.addSource('atlmaps', {
-        type: 'raster',
-        tiles: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:r9jps&service=WMS&request=GetMap&styles=&format=image/png&transparent=true&version=1.1.1&width=256&height=256&srs=EPSG:3857&bbox={bbox-epsg-3857}'],
-        tileSize: 256,
-        attribution: '© ATLMaps',
-      });
-
-      _map.addLayer({
-        id: 'atlmaps-layer',
-        type: 'raster',
-        source: 'atlmaps',
-        layout: {
-          visibility: 'none'
-        }
-      });
-
+      addMapLayers(_map);
       setMap(_map);
       setMapLoaded(true);
     });
@@ -97,69 +46,55 @@ const Map = ({ map, setMap, setMapLoaded }: Props) => {
     };
   }, [setMap, setMapLoaded]);
 
-  const toggleLayer = (layerName: string) => {
-    if (!map) return;
-
-    const layers = ['google-satellite-layer', 'usgs-topo-layer', 'atlmaps-layer'];
-
-    layers.forEach((layer) => {
-      map.setLayoutProperty(layer, 'visibility', 'none');
+  const addMapLayers = (_map: maplibregl.Map) => {
+    // Add all the layers here
+    // Google Satellite
+    _map.addSource('google-satellite', {
+      type: 'raster',
+      tiles: ['https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'],
+      tileSize: 256,
+      attribution: '© Google',
+    });
+    _map.addLayer({
+      id: 'google-satellite-layer',
+      type: 'raster',
+      source: 'google-satellite',
+      layout: { visibility: 'none' }
     });
 
-    if (layerName !== 'custom') {
-      map.setLayoutProperty(`${layerName}-layer`, 'visibility', 'visible');
-    }
+    // USGS Topo
+    _map.addSource('usgs-topo', {
+      type: 'raster',
+      tiles: ['https://basemap.nationalmap.gov/arcgis/services/USGSTopo/MapServer/WMSServer?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=0&styles=default'],
+      tileSize: 256,
+      attribution: '© USGS',
+    });
+    _map.addLayer({
+      id: 'usgs-topo-layer',
+      type: 'raster',
+      source: 'usgs-topo',
+      layout: { visibility: 'none' }
+    });
 
-    setActiveLayer(layerName);
-    setIsDropdownOpen(false);
+    // ATLMaps
+    _map.addSource('atlmaps', {
+      type: 'raster',
+      tiles: ['https://geoserver.ecds.emory.edu/ATLMaps/gwc/service/wms?layers=ATLMaps:r9jps&service=WMS&request=GetMap&styles=&format=image/png&transparent=true&version=1.1.1&width=256&height=256&srs=EPSG:3857&bbox={bbox-epsg-3857}'],
+      tileSize: 256,
+      attribution: '© ATLMaps',
+    });
+    _map.addLayer({
+      id: 'atlmaps-layer',
+      type: 'raster',
+      source: 'atlmaps',
+      layout: { visibility: 'none' }
+    });
   };
 
   return (
     <div className="relative">
       <div ref={mapContainerRef} className={`h-[calc(100vh-${topBarHeight})]`}></div>
-      <div className="absolute top-4 right-4">
-        <button
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          className="bg-white p-2 rounded-md shadow-md"
-        >
-          <FontAwesomeIcon icon={faLayerGroup} />
-          <span className="sr-only">Toggle layer menu</span>
-        </button>
-        {isDropdownOpen && (
-          <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-              <button
-                onClick={() => toggleLayer('custom')}
-                className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left ${activeLayer === 'custom' ? 'bg-gray-200' : ''}`}
-                role="menuitem"
-              >
-                Default Layer
-              </button>
-              <button
-                onClick={() => toggleLayer('google-satellite')}
-                className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left ${activeLayer === 'google-satellite' ? 'bg-gray-200' : ''}`}
-                role="menuitem"
-              >
-                Satellite Layer
-              </button>
-              <button
-                onClick={() => toggleLayer('usgs-topo')}
-                className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left ${activeLayer === 'usgs-topo' ? 'bg-gray-200' : ''}`}
-                role="menuitem"
-              >
-                USGS Topo Layer
-              </button>
-              <button
-                onClick={() => toggleLayer('atlmaps')}
-                className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left ${activeLayer === 'atlmaps' ? 'bg-gray-200' : ''}`}
-                role="menuitem"
-              >
-                ATLMaps Layer
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
+      <MapSwitcher map={map} />
     </div>
   );
 };
