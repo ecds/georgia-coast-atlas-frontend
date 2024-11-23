f42bd93597b50be47df19a69514d6a2b9fbb33e1
Jay Varner <jayvarner@gmail.com>
2024-11-23 18:55:09 -0500

    Vs dev (#19)
    * removed defer
    
    * resolved warnings
    
    ---------
    
    Co-authored-by: Varshith <varshith.sai0605@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,12 @@ export default defineConfig({
   plugins: [
     remix({
       future: {
-        v3_singleFetch: true, 
+
+        v3_singleFetch: true,
+        v3_fetcherPersist: true, 
+        v3_lazyRouteDiscovery: true, 
+        v3_relativeSplatPath: true, 
+        v3_throwAbortReason: true,  
       },
     }),
     tsconfigPaths(),
