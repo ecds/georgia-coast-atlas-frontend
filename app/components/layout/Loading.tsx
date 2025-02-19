f46eddee95eba3385bf3b8e7053bc736b2e6ade5
Jay Varner <jayvarner@gmail.com>
2025-02-19 10:25:57 -0500

    Fix position of loading overlay.

--- app/components/layout/Loading.tsx
+++ app/components/layout/Loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
   const navigation = useNavigation();
   return (
     <div
-      className={`absolute flex items-center text-center h-screen w-screen top-0 z-50 bg-black/50 transition-opacity duration-700 ${navigation.state == "loading" ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+      className={`fixed flex items-center text-center h-screen w-screen top-0 z-50 bg-black/50 transition-opacity duration-700 ${navigation.state == "loading" ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
     >
       <div className="relative grow text-6xl text-white">
         <FontAwesomeIcon
