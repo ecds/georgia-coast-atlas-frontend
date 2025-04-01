08a914dd0af26d1b47974642722dea6c053f21bb
Jay Varner <jayvarner@gmail.com>
2025-04-01 10:10:19 -0400

    Fix remote script.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,10 +6,10 @@ echo "Building"
 npm run build
 
 echo "Copying Files"
-files=("./build", "./package.json", "./server.*", "./run.sh")
+files=("./build" "./package.json" "./server.*" "./run.sh")
 for file in "${files}"; do
   rsync -avze "ssh" ${file} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 done
 
-echo "Installing NPM Packages"
-ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_PATH}; npm install"
+echo "Running Remote Script"
+ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_PATH}; chmod +x run.sh; ./run.sh" &
