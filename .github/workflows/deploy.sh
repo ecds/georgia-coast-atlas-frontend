6c7c9ddf9666933ecdfcb91c3722b7b45dfa5081
Jay Varner <jayvarner@gmail.com>
2025-04-01 09:50:53 -0400

    Rework SSH Key

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 echo "Running deploy script"
-echo $DH_SSH_KEY >.key
-chmod 600 .key
 
 echo "Building"
 npm run build
@@ -10,8 +8,8 @@ npm run build
 echo "Copying Files"
 files=("./build", "./package.json", "./server.*", "./run.sh")
 for file in "${files}"; do
-  rsync -avze "ssh -o StrictHostKeyChecking=no -i .key" "$file" ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+  rsync -avze "ssh" ${file} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 done
 
 echo "Installing NPM Packages"
-ssh -o StrictHostKeyChecking=no -i .key ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_PATH}; npm install"
+ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_PATH}; npm install"
