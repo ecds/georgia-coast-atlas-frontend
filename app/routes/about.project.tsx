bcb8acb56dc6bb052691f963bfed497143a5a9d4
Jay Varner <jayvarner@gmail.com>
2025-04-30 09:11:25 -0400

    Merge about project route from dev

--- app/routes/about.project.tsx
+++ app/routes/about.project.tsx
@@ -1,35 +1,17 @@
 import { dataHosts } from "~/config";
 import { useLoaderData } from "@remix-run/react";
-import { pageMetaDefaults } from "~/utils/pageMetadata";
-import wpCssUrl from "~/styles/about.css?url";
+import "~/styles/about.css";
 import type { TWordPressData } from "~/types";
-import type { MetaFunction } from "@remix-run/react";
-import { LinksFunction } from "@remix-run/node";
 
 export const loader = async () => {
   const wpResponse = await fetch(
-    `https://${dataHosts.wordPress}/wp-json/wp/v2/pages/?slug=about`
+    `https://${dataHosts.wordPress}/wp-json/wp/v2/pages/?slug=about-project`
   );
 
   const wpData: TWordPressData[] = await wpResponse.json();
 
   return { wpData: wpData[0] };
 };
-
-export const links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: wpCssUrl }
-  ]
-}
-
-export const meta: MetaFunction = () => {
-  return [
-    {
-      ...pageMetaDefaults,
-      title: "About the Project: Georgia Coast Atlas"
-    }
-  ]
-}
 
 const ProjectPage = () => {
   const { wpData } = useLoaderData<{ wpData: TWordPressData }>();
