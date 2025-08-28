a2b6be172fa48429f2b1d227a463afd5522a1f59
Jay Varner <jayvarner@gmail.com>
2025-08-28 16:14:59 -0400

    Update deploy to set NODE_ENV by branch

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,6 +5,11 @@ echo "Running deploy script"
 REMOTE_PATH=$([ "$BRANCH" == "main" ] && echo $REMOTE_PATH_PROD || echo $REMOTE_PATH_DEV)
 RESTART_COMMAND=$([ "$BRANCH" == "main" ] && echo $PROD_RESTART_COMMAND || echo $DEV_RESTART_COMMAND)
 
+if [ $BRANCH = "main" ]; then
+  export NODE_ENV="production"
+else
+  export NODE_ENV="develop"
+fi
 
 echo "Building"
 npm run build
